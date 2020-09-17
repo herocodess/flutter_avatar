726967cf6c0b7ff0e8a0ee1efdafcbd20fb123da
mhero007 <momohhero2@gmail.com>
2020-09-17 23:40:04 +0100

    added widget parameters

--- lib/flutter_avatar.dart
+++ lib/flutter_avatar.dart
@@ -1,7 +1,30 @@
 library flutter_avatar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+enum Shapes {
+  Circle,
+  Rectangle,
+}
+
+class FlutterAvatar extends StatelessWidget {
+  const FlutterAvatar(
+      {Key key,
+      this.shape,
+      this.child,
+      this.backgroundColor,
+      this.backgroundImage,
+      this.backgroundImageError})
+      : assert(shape != null),
+        super(key: key);
+  final Shapes shape;
+  final Widget child;
+  final Color backgroundColor;
+  final ImageProvider backgroundImage;
+  final ImageErrorListener backgroundImageError;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
