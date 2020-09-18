ffe5ff4d336a11a217813110b91340c7d725bcd1
mhero007 <momohhero2@gmail.com>
2020-09-18 13:59:07 +0100

    added circle methods

--- lib/flutter_avatar.dart
+++ lib/flutter_avatar.dart
@@ -1,8 +1,9 @@
 library flutter_avatar;
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-enum Shapes {
+enum BoxShape {
   Circle,
   Rectangle,
 }
@@ -13,18 +14,46 @@ class FlutterAvatar extends StatelessWidget {
       this.shape,
       this.child,
       this.backgroundColor,
+      this.foregroundColor,
       this.backgroundImage,
       this.backgroundImageError})
       : assert(shape != null),
         super(key: key);
-  final Shapes shape;
+  final BoxShape shape;
   final Widget child;
   final Color backgroundColor;
+  final Color foregroundColor;
   final ImageProvider backgroundImage;
   final ImageErrorListener backgroundImageError;
+  final double radius = 0.0;
+  final double minRadius = 0.0;
+  final double maxRadius = 0.0;
+  static const double _defaultRadius = 20.0;
+  static const double _defaultMinRadius = 0.0;
+  static const double _defaultMaxRadius = double.infinity;
+
+  double get _minDiameter {
+    if (radius == null && minRadius == null && maxRadius == null) {
+      return _defaultRadius * 2.0;
+    }
+    return 2.0 * (radius ?? minRadius ?? _defaultMinRadius);
+  }
+
+  double get _maxDiameter {
+    if (radius == null && minRadius == null && maxRadius == null) {
+      return _defaultRadius * 2.0;
+    }
+    return 2.0 * (radius ?? maxRadius ?? _defaultMaxRadius);
+  }
+
+  double shapeCircle() {}
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    assert(debugCheckHasMediaQuery(context));
+    final ThemeData theme = Theme.of(context);
+    TextStyle textStyle =
+        theme.primaryTextTheme.subtitle1.copyWith(color: foregroundColor);
+    Color eBackgroundColor = backgroundColor;
+    if (shape == BoxShape.Circle) {}
   }
 }
